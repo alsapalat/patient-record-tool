aa00e6be0a256fa5810aaa6511f52c459fa17d4c
Adam Lee Apalat <alsapalat@multisyscorp.com>
2025-10-22 01:41:28 +0800

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/patient-agg-record/',
+  base: '/patient-record-tool/',
   plugins: [
     react({
       babel: {
